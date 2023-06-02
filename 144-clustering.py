133c6ff2aa57674eaf01640bf712ddb5b9b16a78
abtin-otw <63191995+abtin-otw@users.noreply.github.com>
2023-06-02 08:56:56 -0700

    Add files via upload

---
+++ 144-clustering.py
@@ -0,0 +1,29 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from k_means_constrained import KMeansConstrained
+
+
+df = pd.read_fwf('Points.txt')
+
+data = df.to_numpy()
+
+x = data[:,0]
+y = data[:,1]
+z = data[:,2]
+
+clf = KMeansConstrained(
+    n_clusters=410063,
+    size_min=143,
+    size_max=145,
+    random_state=0
+)
+
+clf.fit_predict(data)
+df['cluster'] = clf.labels_
+
+#print(df['cluster'].value_counts())
+
+ax = plt.axes(projection='3d')
+ax.scatter(x, y, z, c = df['cluster'])
+plt.show()
